f85e434cdd85fdecb2b16b09eab8acfd83d54de9
ohsam95 <ohsam95@naver.com>
2020-06-29 15:40:14 +0900

    노드 값 전송까지 완료

--- src/socket/Node1Websocket.java
+++ src/socket/Node1Websocket.java
@@ -1,10 +1,8 @@
 package socket;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.websocket.OnClose;
-
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -12,15 +10,12 @@ import javax.websocket.server.ServerEndpoint;
 
 import com.google.gson.GsonBuilder;
 import com.node1.model.Block;
-import com.node1.util.Mine;
-
-import jdk.internal.dynalink.beans.StaticClass;
 
 
 
 // WebSocket의 호스트 주소 설정
 @ServerEndpoint("/websocket")
-public class Websocket {
+public class Node1Websocket {
 //	List<String> messageArray = new ArrayList<String>();
 	ArrayList<Block> blockchain = new ArrayList<Block>();
 	int i = 1;
@@ -36,42 +31,35 @@ public class Websocket {
 // WebSocket으로 메시지가 오면 요청되는 함수
 	@OnMessage
 	public String handleMessage(String message) {
-		// 메시지 내용을 콘솔에 출력한다.
-				System.out.println("10분간의 거래데이터 : " + message);
-				
-				
-				
-				//채굴 과정
-				// 지금 문제 - 첫번째는 잘됨 -> 두번째부터는 이전의 블록과 연결이 안됨
-				
-				//블록들을 담을 ArrayList 생성 
-			 	
+				//채굴 과정 			 	
 			    int difficulty = 1; //난이도 설정
 
-				
 				//초기 블록 생성
 			    if (blockchain.isEmpty()) {
 					blockchain.add(new Block(message,"0"));
 					System.out.println("1번 블록을 채굴합니다");
-					blockchain.get(0).mineBlock(difficulty);					
+					blockchain.get(0).mineBlock(difficulty);			
+					
 				}else {
 					//이후 블록 생성
-//					for (int i = 1; i <= 5; i++) {
+
 					blockchain.add(new Block(message, blockchain.get(blockchain.size()-1).hash));
 					System.out.println(i+1+"번 블록을 채굴합니다");
 					blockchain.get(i).mineBlock(difficulty);
-					i= i+1;
-//				}
-					//대박... 나 성공했어... 이제 블록을 보내주는 알고리즘만들고 다른 노드들 만들고 경쟁하는 알고리즘 만들고!! 
+					i= i+1; 
 				}
-				//전체 블록을 출력
-				String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+			  
+			  //전체 블록을 출력
+			    String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+			    System.out.println("1번 노드 채굴성공");
 				System.out.println("블록체인 리스트 : ");
 				System.out.println(blockchainJson);
-		
+						
+				// 이제 블록을 보내주는 알고리즘만들고 다른 노드들 만들고 경쟁하는 알고리즘 만들고!!
+						
 
-// 에코 메시지를 브라우저에 보낸다.
-		return "test1";
+				// 리턴
+		return blockchainJson;
 	}
 
 // WebSocket과 브라우저가 접속이 끊기면 요청되는 함수
